d7041d98dffa2647b5129aec3add4e6199413156
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-11-28 20:25:51 +0330

    add connectionObserver to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    debugImplementation(project(":internetconnectionobserver"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
